c89d693c1a6d6f111f188d34ca2a2884ca10c36d
Matt Moore <matt@mattmoore.io>
2020-03-13 23:06:22 -0500

    Rename test to spec.

--- src/test/kotlin/io/mattmoore/kimage/KImageSpec.kt
+++ src/test/kotlin/io/mattmoore/kimage/KImageSpec.kt
@@ -3,7 +3,7 @@ package io.mattmoore.kimage
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
-class KImageTests: StringSpec({
+class KImageSpec: StringSpec({
     "loads pixel data from an image" {
         1 shouldBe 1
     }
